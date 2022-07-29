79123d587e29484955f472b4efaf056c507fd8ee
arko <arkajyotisaha891@gmail.com>
2022-07-29 07:09:36 +0530

    cahnges

---
+++ recr_back/permutation1.cpp
@@ -0,0 +1,19 @@
+// Problem Link - https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/recursion-and-backtracking/permutation-i-official/ojquestion
+//orddering dont matter here
+/* By Arkajyoti Saha */
+#include<bits/stdc++.h>
+using namespace std;
+void file_i_o(){
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+}
+//######################################################################################################################################################################
+
+int main(){
+    file_i_o();
+    int number;
+    cin>>number;
+    cout<<"number="<<number<<endl;
+}
---
+++ recr_back/template.cpp
@@ -0,0 +1,23 @@
+// Problem Link - 
+/* By Arkajyoti Saha */
+#include<bits/stdc++.h>
+using namespace std;
+void file_i_o(){
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+}
+//######################################################################################################################################################################
+
+int main(){
+    file_i_o();
+    int number;
+    cin>>number;
+    cout<<"number="<<number<<endl;
+}
+
+
+
+
+
