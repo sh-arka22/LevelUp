c30a3487160e604facf75a9b04bc7c4aea066bf4
arko <arkajyotisaha891@gmail.com>
2022-10-04 08:59:16 +0530

    DPonGrids

--- DP/minimumFaillingPath.cpp
+++ DP/minimumFaillingPath.cpp
@@ -55,13 +55,29 @@ int recc(vector<vector<int>>& matrix, int R, int C){
     }
     return dp[R][C];
 }
+// int print(vector<vector<int>>& matrix, int r, int c){
+//     if(r==0) return matrix[r][c];
 
-int minFallingPathSum(vector<vector<int>>& matrix) {
+//     int mini = (int)1e9;
+//     for(auto &[dr,dc]: dir){
+//         int x = r+dr, y = c+dc;
+//         if(x<0 or y<0 or x>=matrix.size() or y>=matrix[0].size()) continue;
+//         mini = min(mini,recc(matrix,x,y));
+//     }
+//     return mini+matrix[r][c];
+// }
+
+int minFallingPathSum(vector<vector<int>> matrix) {
     int r = matrix.size(), c = matrix[0].size();
     dp.resize(r,vector<int>(c));
     int mini = (int)1e9;
+    int si, sj;
     for(int j=c-1;j>=0;j--){
-        mini = min(mini,recc(matrix,r-1,j));
+        int call = recc(matrix,r-1,j);
+        if(call<mini){
+            mini = call;
+            int si = r-1, sj = j;
+        }
     }
     return mini;
 }
@@ -73,8 +89,15 @@ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 	file_i_o();
 	// START //
-    
-
-    
+    vector<vector<int>>matrix = {{1, 2, 3},
+                                { 4, 5, 6},
+                                { 7, 8, 9}};
+    cout<<minFallingPathSum(matrix)<<endl;
+    for(auto it:dp){
+        for(int i:it){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
 	return 0;
 }
